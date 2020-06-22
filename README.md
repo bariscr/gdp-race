e3e141e10ac7924fdeae39c0cbc6f7e353e95dc6
Barış Uçar <barisucar@Barss-MBP.mynet>
2020-06-22 10:34:57 +0300

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gdp-race
 Changes in GDP rankings.
+
+1. Data is acquired from World Bank:
+
+https://data.worldbank.org/indicator/NY.GDP.PCAP.CD?end=2018&start=1977
+
+2. Data is transformed to long format and uploaded to github.
+
+3. Do the ranking, plot and animation in the code file.
---
+++ gdp-race.R
@@ -0,0 +1,40 @@
+# load the data
+load(url("https://github.com/bariscr/data/raw/master/gdp_per_capita_world.Rdata"))
+################################################################################
+# transform data - make the NA's 0
+library(tidyverse)
+gdp_data <- gdp_per_capita_world %>% 
+  mutate(gdp_pc = 
+           case_when(is.na(gdp_pc) == TRUE ~ 0,
+                     TRUE ~ gdp_pc))
+# give the ranks
+gdp_data2 <- gdp_data %>%
+  group_by(year) %>%
+  mutate(rank = min_rank(-gdp_pc)) %>%
+  ungroup()
+# select top countries
+top_countries <- gdp_data2 %>% 
+  filter(rank <= 10) %>%
+  select(country_name, year, rank) %>% distinct()
+
+# plot
+library(gganimate)
+library(ggdark)
+p <- ggplot(top_countries, aes(rank, group = country_name, 
+                               fill = as.factor(country_name))) +
+  geom_text(aes(y = 0, label = paste0(rank, ". ", country_name, " ")),
+            hjust = "center", fontface = "bold", size = 8) +
+  coord_flip(clip = "off", expand = FALSE) +
+  scale_x_reverse() +
+  guides(color = FALSE, fill = FALSE) +
+  dark_theme_minimal() +
+  labs(title='{closest_state}', caption="Data: World Bank", x = "", y = "") +
+  theme(plot.title = element_text(hjust = 0, size = 40),
+        axis.ticks.y = element_blank(),  
+        axis.text.y  = element_blank(),  
+        plot.margin = margin(1,1,1,4, "cm")) +
+  transition_states(year, transition_length = 4, state_length = 1) +
+  ease_aes('cubic-in-out')
+anim <- animate(p, fps = 25, duration = 30, width = 800, height = 600)
+
+anim_save("output.gif", anim)
